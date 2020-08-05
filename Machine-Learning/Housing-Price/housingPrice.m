a31e46b9f7149971bcbe6e4bf323838f68d8df53
Chunjeho <jejehoho3317@gmail.com>
2020-08-05 17:15:09 +0900

    edit housingPrice.m source code

--- Machine-Learning/Housing-Price/housingPrice.m
+++ Machine-Learning/Housing-Price/housingPrice.m
@@ -2,16 +2,19 @@ load featuresX.dat
 load priceY.dat
 
 x = featuresX(:, 1);
+x0 = ones(size(x)(:,1),1);
+x = [[x0] x];
 y = priceY;
 
 theta = pinv(x'*x)*(x'*y);
-h = theta*x;
+h = theta(2)*x(:,2) + theta(1);
 
-plot(x, y, "*");
+plot(x(:,2), y, "*");
 hold on;
-plot(x, h);
+plot(x(:,2), h);
 xlabel("Size (feet^2)");
 ylabel("Price ($)");
 legend("examples", "hypothesis");
 title("Housing-Price");
+
 print -dpng "Housing-Price.png";
